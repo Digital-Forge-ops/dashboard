a5eae8fb3a05633a4f11ed8f07b386cb77514b50
Digital-Forge-ops <dan@digitalforgeai.co>
2025-10-19 01:23:18 -0400

    Change server port from 9001 to 9000

--- vite.config.js
+++ vite.config.js
@@ -126,7 +126,7 @@ export default defineConfig(({ command, mode }) => {
     base,
     envDir: "..",
     server: {
-      port: 9001,
+      port: 9000,
       fs: {
         allow: [searchForWorkspaceRoot(process.cwd()), "../.."],
       },
